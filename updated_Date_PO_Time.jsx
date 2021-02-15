7fad22445d69847bf15c811a809dfb389ed83006
Daniel Cooper <d.cooper2812@gmail.com>
2021-02-15 17:04:39 +0000

    Refactoring Old Code Script To Streamline

--- updated_Date_PO_Time.jsx
+++ updated_Date_PO_Time.jsx
@@ -1,32 +1,47 @@
 //! top info functions
 //this function calles for the date and updates the proof with it. It relies on the date text on the proof being called 'Ord_DATE'
 function setDate() {
-    var date = new Date();
-    var day = date.getDate();
-    var monthIndex = date.getMonth();
-    var year = date.getFullYear();
+  var date = new Date();
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
 
-    // priocess the month
-    monthIndex = monthIndex + 1 // it returns the month as january being 0, so this just changes it so jan = 1
+  // priocess the month
+  monthIndex = monthIndex + 1; // it returns the month as january being 0, so this just changes it so jan = 1
 
-        if(monthIndex < 10) { // if it's less then 10 make it display as 01 instead of 1
-            monthIndex = '0' + monthIndex
-        } 
-        
-    // update the document with the relevant info.
-    app.activeDocument.textFrames.getByName ("Ord_DATE").contents = day + '/' + monthIndex + '/' + year;
+  if (monthIndex < 10) {
+    // if it's less then 10 make it display as 01 instead of 1
+    monthIndex = "0" + monthIndex;
+  }
 
-    return
-    }
-    
-    function setORderNameAndNumber() {
-    
-    var FileName = app.activeDocument.name.toUpperCase();
-    
-    
-    // Regular Expressions to Parse Order Number and name
-    
-    // Name
+  // update the document with the relevant info.
+  app.activeDocument.textFrames.getByName("Ord_DATE").contents =
+    day + "/" + monthIndex + "/" + year;
+
+  return;
+}
+
+function setORderNameAndNumber() {
+  var FileName = app.activeDocument.name.toUpperCase();
+
+  // Regular Expressions to Parse Order Number and name
+
+  const regexArr = [
+    /PROOF/g,
+    /VISUAL/g,
+    /V[0-9]/g,
+    /[-_]/g,
+    /^.*[0-9]/g,
+    /^\ /,
+    ".PDF",
+  ];
+
+  for (var i = 0; i < regexArr.length; i++) {
+    FileName = FileName.replace(regexArr[i], "");
+  }
+
+  /*
+
     var newName = FileName.replace(".PDF","");
     newName = newName.replace(/ PROOF/g, "");
     newName = newName.replace(/ VISUAL/g, "");
@@ -34,22 +49,22 @@ function setDate() {
     newName = newName.replace(/[-_]/g, "");
     newName = newName.replace(/^.*[0-9]/g,"");
     newName = newName.replace(/^\ /,"");
-    // Number
-    var newNumber = newName.replace(newName, "");
-    
-    // Update Proof to new Varables
-    app.activeDocument.textFrames.getByName ("ORDER_NAME").contents = newName;
-    
-    
-    //if the order number is blank call it visual
-    if (newNumber == "") {
-      app.activeDocument.textFrames.getByName ("PO_NUMBER").contents = 'VISUAL';
-    }else {
-      app.activeDocument.textFrames.getByName ("PO_NUMBER").contents = newNumber;
-    }
-    
-    
-    }
 
-    setDate()
-    setORderNameAndNumber()
+    */
+
+  // Number
+  var newNumber = FileName.replace(FileName, "");
+
+  // Update Proof to new Varables
+  app.activeDocument.textFrames.getByName("ORDER_NAME").contents = FileName;
+
+  //if the order number is blank call it visual
+  if (newNumber == "") {
+    app.activeDocument.textFrames.getByName("PO_NUMBER").contents = "VISUAL";
+  } else {
+    app.activeDocument.textFrames.getByName("PO_NUMBER").contents = newNumber;
+  }
+}
+
+setDate();
+setORderNameAndNumber();
