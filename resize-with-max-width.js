8425b13b99be1d089d9172c5fa72fc484c90a9f8
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-02-17 23:27:22 +0000

    branch check

--- resize-with-max-width.js
+++ resize-with-max-width.js
@@ -1,76 +1,59 @@
 var docRef = app.activeDocument;
 
-// select every group item 
-var count = 0;
+var fullWidth = 0
 
-
-// if there's no groups make the paths groups
-alert(docRef.groupItems.length)
 if (docRef.groupItems.length <= 0) {
 
     for (var i = 0; i < docRef.pathItems.length; i++) {
       
-        /*
+        alert('trig')
+        
         docRef.pathItems[i].selected = true
-        app.executeMenuCommand('group')
-        docRef.pathItems[i].selected = false
-        */
-    
 
-        
+        // Space out the items, WIP sort of works....
+        docRef.pathItems[i].position = [fullWidth + (docRef.pathItems[i -1].width + 100), 0]
+        fullWidth += docRef.pathItems[i].width + 100
 
-        //app.executeMenuCommand('deselectall')
-        
+        docRef.pathItems[i].selected = false   
+
     }
     
 }
 
 
 
+// Loop through every group in the file 
 for (var i = 0; i < docRef.groupItems.length; i++) {
-    count++
-    
-   
-
-    // calculate difference
-
-
-    // get these from the user
+  
+    // These are hard coded at the moment for debugging. this will be a user input.
     var maxWidth = 280
     var maxHeight = 280
 
+    var increase = convertMMtoPT(maxHeight) - docRef.groupItems[i].height // this calculates the increase.
+    var increasePercent = (increase / docRef.groupItems[i].height) * 100 // this converts the increase into a percentage
+    increasePercent += 100 // add 100, so the increase is 140% rather than 40% which is too small.
 
-    var increase = convertMMtoPT(maxHeight) - docRef.groupItems[i].height
-
-    var increasePercent = (increase / docRef.groupItems[i].height) * 100
-   
-
-    // add 100 to get the added percent
-    increasePercent = increasePercent + 100
-
-    docRef.groupItems[i].resize(increasePercent, increasePercent)
+    docRef.groupItems[i].resize(increasePercent, increasePercent) // Change the objects size
 
     // Check the width to see if they're over max width
-
     if (docRef.groupItems[i].width > convertMMtoPT(maxWidth)) {
         
+        // Does the same as before, but this time it's checking the width isn't too big.
         increase = convertMMtoPT(maxWidth) - docRef.groupItems[i].width
-
         increasePercent = (increase / docRef.groupItems[i].width) * 100
-
-
-        // add 100 to get the added percent
-        increasePercent = increasePercent + 100
+        increasePercent += 100
 
         docRef.groupItems[i].resize(increasePercent, increasePercent)
     }
+
+    // this renames the group, not useful at the moment. 
     app.activeDocument.groupItems[i].name = 'I Am Group ' + i
     //app.activeDocument.groupItems[i].locked = true
 
 }
 
 
-// work out the difference.
+// * Supporting Functions
 
 function convertMMtoPT(input) {
     return input / 0.352778
@@ -78,8 +61,4 @@ function convertMMtoPT(input) {
 function convertPTtoMM(input) {
     var output = input * 0.352778
     return output
-}
-function getIncreasePercentage(increase, width) {
-    // make this a function
-
 }
