47343fd928dd3e623f3a5f891bc88a47fe75ac23
Daniel Cooper <d.cooper2812@gmail.com>
2021-02-17 13:42:49 +0000

    working

--- resize-with-max-width.js
+++ resize-with-max-width.js
@@ -7,7 +7,55 @@ var count = 0;
 
 for (var i = 0; i < docRef.pathItems.length; i++) {
     count++
-    docRef.pathItems[i].resize(200, 200)
+    
+
+    // calculate difference
+
+
+    // get these from the user
+    var maxWidth = 280
+    var maxHeight = 280
+
+
+    var increase = convertMMtoPT(maxHeight) - docRef.pathItems[i].height
+
+    var increasePercent = (increase / docRef.pathItems[i].height) * 100
+   
+
+    // add 100 to get the added percent
+    increasePercent = increasePercent + 100
+
+    docRef.pathItems[i].resize(increasePercent, increasePercent)
+
+    // Check the width to see if they're over max width
+
+    if (docRef.pathItems[i].width > convertMMtoPT(maxWidth)) {
+        
+        increase = convertMMtoPT(maxWidth) - docRef.pathItems[i].width
+
+        increasePercent = (increase / docRef.pathItems[i].width) * 100
+
+
+        // add 100 to get the added percent
+        increasePercent = increasePercent + 100
+
+        docRef.pathItems[i].resize(increasePercent, increasePercent)
+    }
+
+
 }
 
-alert(count)
+
+// work out the difference.
+
+function convertMMtoPT(input) {
+    return input / 0.352778
+}
+function convertPTtoMM(input) {
+    var output = input * 0.352778
+    return output
+}
+function getIncreasePercentage(increase, width) {
+    // make this a function
+
+}
