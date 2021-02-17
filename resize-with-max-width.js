7fa610710e786318b3c2f2e8505a42b2911059f9
Daniel Cooper <d.cooper2812@gmail.com>
2021-02-17 17:03:44 +0000

    .

--- resize-with-max-width.js
+++ resize-with-max-width.js
@@ -1,13 +1,36 @@
 var docRef = app.activeDocument;
 
-// select every path item 
+// select every group item 
 var count = 0;
 
 
+// if there's no groups make the paths groups
+alert(docRef.groupItems.length)
+if (docRef.groupItems.length <= 0) {
 
-for (var i = 0; i < docRef.pathItems.length; i++) {
+    for (var i = 0; i < docRef.pathItems.length; i++) {
+      
+        /*
+        docRef.pathItems[i].selected = true
+        app.executeMenuCommand('group')
+        docRef.pathItems[i].selected = false
+        */
+    
+
+        
+
+        //app.executeMenuCommand('deselectall')
+        
+    }
+    
+}
+
+
+
+for (var i = 0; i < docRef.groupItems.length; i++) {
     count++
     
+   
 
     // calculate difference
 
@@ -17,31 +40,32 @@ for (var i = 0; i < docRef.pathItems.length; i++) {
     var maxHeight = 280
 
 
-    var increase = convertMMtoPT(maxHeight) - docRef.pathItems[i].height
+    var increase = convertMMtoPT(maxHeight) - docRef.groupItems[i].height
 
-    var increasePercent = (increase / docRef.pathItems[i].height) * 100
+    var increasePercent = (increase / docRef.groupItems[i].height) * 100
    
 
     // add 100 to get the added percent
     increasePercent = increasePercent + 100
 
-    docRef.pathItems[i].resize(increasePercent, increasePercent)
+    docRef.groupItems[i].resize(increasePercent, increasePercent)
 
     // Check the width to see if they're over max width
 
-    if (docRef.pathItems[i].width > convertMMtoPT(maxWidth)) {
+    if (docRef.groupItems[i].width > convertMMtoPT(maxWidth)) {
         
-        increase = convertMMtoPT(maxWidth) - docRef.pathItems[i].width
+        increase = convertMMtoPT(maxWidth) - docRef.groupItems[i].width
 
-        increasePercent = (increase / docRef.pathItems[i].width) * 100
+        increasePercent = (increase / docRef.groupItems[i].width) * 100
 
 
         // add 100 to get the added percent
         increasePercent = increasePercent + 100
 
-        docRef.pathItems[i].resize(increasePercent, increasePercent)
+        docRef.groupItems[i].resize(increasePercent, increasePercent)
     }
-
+    app.activeDocument.groupItems[i].name = 'I Am Group ' + i
+    app.activeDocument.groupItems[i].locked = true
 
 }
 
