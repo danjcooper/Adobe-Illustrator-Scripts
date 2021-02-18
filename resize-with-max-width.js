e61a0cdf62b7680c633b4312a6ec9b7986469b58
Daniel Cooper <d.cooper2812@gmail.com>
2021-02-18 14:39:49 +0000

    working wih tiling and UI

--- resize-with-max-width.js
+++ resize-with-max-width.js
@@ -1,33 +1,149 @@
 var docRef = app.activeDocument;
 
-var fullWidth = 0
 
-if (docRef.groupItems.length <= 0) {
 
-    for (var i = 0; i < docRef.pathItems.length; i++) {
+//! Start of Pop up Box 
+
+
+/*
+Code for Import https://scriptui.joonas.me — (Triple click to select): 
+*/
+
+// DIALOG
+// ======
+var dialog = new Window("dialog"); 
+    dialog.text = "Dialog"; 
+    dialog.orientation = "column"; 
+    dialog.alignChildren = ["right","center"]; 
+    dialog.spacing = 10; 
+    dialog.margins = 16; 
+
+var statictext1 = dialog.add("statictext", undefined, undefined, {name: "statictext1"}); 
+    statictext1.text = "Max Width / Height"; 
+    statictext1.alignment = ["center","center"]; 
+
+// PANEL1
+// ======
+var panel1 = dialog.add("panel", undefined, undefined, {name: "panel1"}); 
+    panel1.text = "Sizing"; 
+    panel1.orientation = "column"; 
+    panel1.alignChildren = ["left","center"]; 
+    panel1.spacing = 10; 
+    panel1.margins = 10; 
+
+// GROUP1
+// ======
+var group1 = panel1.add("group", undefined, {name: "group1"}); 
+    group1.orientation = "row"; 
+    group1.alignChildren = ["left","center"]; 
+    group1.spacing = 10; 
+    group1.margins = 0; 
+
+var statictext2 = group1.add("statictext", undefined, undefined, {name: "statictext2"}); 
+    statictext2.helpTip = "in mm"; 
+    statictext2.text = "Max Width:"; 
+
+var edittext1 = group1.add('edittext {properties: {name: "edittext1"}}'); 
+
+// GROUP2
+// ======
+var group2 = panel1.add("group", undefined, {name: "group2"}); 
+    group2.orientation = "row"; 
+    group2.alignChildren = ["left","center"]; 
+    group2.spacing = 10; 
+    group2.margins = 0; 
+
+var statictext3 = group2.add("statictext", undefined, undefined, {name: "statictext3"}); 
+    statictext3.text = "Max Height:"; 
+
+var edittext2 = group2.add('edittext {properties: {name: "edittext2"}}'); 
+
+// DIALOG
+// ======
+
+var Submit = group1.add("button", undefined, undefined, {name: "Submit"}); 
+Submit.text = "Submit"; 
+
+
+var button2 = dialog.add("button", undefined, undefined, {name: "button2"}); 
+    button2.text = "Cancel"; 
+    button2.alignment = ["center","center"]; 
+
+    
+Submit.onClick = function(){	 
+    var maxWidth = parseInt(edittext1.text)
+    var maxHeight = parseInt(edittext2.text)
+
+    maxWidthMaxHeight(maxWidth, maxHeight)
+    tile()
+    
+    dialog.close();
+}
+
+dialog.show();
+
+
+
+
+
+//! End of Pop up Box 
+
+
+
+
+//! Start of main script
+
+
+
+
+
+function tile() {
+    var fullWidth = 0
+
+    var maxLineWidth = convertMMtoPT(500) // get from user, max width of a line
+    var highest = 0
+    var height = highest
+    
+    for (var i = 0; i < docRef.groupItems.length; i++) {
       
-        alert('trig')
+        if (fullWidth + docRef.groupItems[i].width > maxLineWidth) {
+            height += (highest + convertMMtoPT(25)) // This makes next row heigher with a gap
+            // Reset the heightest height and width.
+            highest = 0
+            fullWidth = 0
+        }
+        //alert('trig')
         
-        docRef.pathItems[i].selected = true
+        //docRef.pathItems[i].selected = true
 
-        // Space out the items, WIP sort of works....
-        docRef.pathItems[i].position = [fullWidth + (docRef.pathItems[i -1].width + 100), 0]
-        fullWidth += docRef.pathItems[i].width + 100
+        
+        docRef.groupItems[i].position = [fullWidth, docRef.groupItems[i].height + height] // Space out the items Evently
+        fullWidth += docRef.groupItems[i].width + convertMMtoPT(25) // the plus here is how much space is added between items. Get this from user input.
 
-        docRef.pathItems[i].selected = false   
+        if (docRef.groupItems[i].height > highest) {
+            highest = docRef.groupItems[i].height
+        }
+
+        //docRef.pathItems[i].selected = false   
+
+       
+        //redraw()
 
     }
-    
 }
 
 
 
+    
+
+
+function maxWidthMaxHeight(maxWidth, maxHeight) {
+    
 // Loop through every group in the file 
 for (var i = 0; i < docRef.groupItems.length; i++) {
   
     // These are hard coded at the moment for debugging. this will be a user input.
-    var maxWidth = 280
-    var maxHeight = 280
+    
 
     var increase = convertMMtoPT(maxHeight) - docRef.groupItems[i].height // this calculates the increase.
     var increasePercent = (increase / docRef.groupItems[i].height) * 100 // this converts the increase into a percentage
@@ -51,6 +167,9 @@ for (var i = 0; i < docRef.groupItems.length; i++) {
     //app.activeDocument.groupItems[i].locked = true
 
 }
+}
+
+
 
 
 // * Supporting Functions
