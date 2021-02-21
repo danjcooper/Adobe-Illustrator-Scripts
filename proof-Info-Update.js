cb312789882f6d0c49fb8846c43f8b25e6ef7956
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-02-21 14:00:42 +0000

    Finished V1

--- proof-Info-Update.js
+++ proof-Info-Update.js
@@ -1,9 +1,34 @@
+/*
+    Author: Daniel Cooper
+    Twitter: @dancoopercodes
+    Github: danjcooper
+
+    Usage ---
+    Update your proof template like bellow
+        Where you want the order number to go, the text should say 'PO NUMBER'
+        The orders name should be 'ORDER NAME'
+        The Date just needs to be any number in the 00/00/02 or 00/00/0000 format, any numbers will work here. It just looks for the pattern.
+
+        The Artworkers name 'AW NAME' You also need to update line 27 of this file to what you want it to say.
+
+        if any of these parameters are missing the program will still run so you can leave any out.
+
+        Also, if more then one thing has the same text in, such as two text boxes include PO NUMBER, both will be updated. This is good for multipage proofs.
+
+        This script also marks up the document, so it will still work for repeats, even when the text boxes don't say what they're named above.
+*/
+
+
 var docRef = app.activeDocument;
 var textFrames = app.activeDocument.textFrames
+
+var artworker = 'D COOPER' // Update me to your name.
 
 var dateRegex = /\d+\/\d+\/\d+/gi
 var approvalDateRegex = /\d+\/\d+/g
 
+// Start of Main Function
+// This is what gets executed when you run the script.
 
 var orderInfo = parseNameAndNumber(app.activeDocument.name)
 
@@ -22,15 +47,24 @@ for (var i = 0; i < docRef.textFrames.length; i++) {
         textFrames[i].name = 'DATE';
 
     } else if (approvalDateRegex.test(textFrames[i].contents) || textFrames[i].name == 'APPROVAL DATE') {
-        textFrames[i].contents = genDate()
+        textFrames[i].contents = approvalDate(genDate())
         textFrames[i].name = 'APPROVAL DATE';
         
     } else if (textFrames[i].contents == 'AW NAME' || textFrames[i].name == 'AW NAME') {
-        textFrames[i].contents = 'D COOPER';
+        textFrames[i].contents = artworker;
         textFrames[i].name = 'AW NAME';
     }
 }
 
+// End of main function.
+
+// Supporting functions, these are called by the main function and don't execute alone.
+
+function approvalDate(date) {
+    var lastDash = date.lastIndexOf('/') //  this finds the last time the '/' in the string. 
+    date = date.slice(0,lastDash) // slice uses the index as it's last argument
+    return date
+}
 
 
 // Parse order name and number
@@ -60,7 +94,7 @@ function parseNameAndNumber(fileName) {
     output.orderName = splitRemoved.replace(/,/g, ' ')
 
     return output
-}
+};
 
 
 // Get the Date
@@ -70,7 +104,7 @@ function genDate() {
     var month = date.getMonth() + 1
     var year = date.getFullYear()
     return day + '/' + month + '/' + year
-}
+};
 
 function isInArr(input, arr) {
     for (var i = 0 ; i < arr.length ; i++) {
@@ -79,8 +113,4 @@ function isInArr(input, arr) {
         }
     }
     return false
-}
-
-
-
-
+};
